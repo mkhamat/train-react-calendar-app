80054ac32e4d0b16230b899610451f2772cda2ff
mkhamat <myxa@xakep.org>
2020-10-21 23:57:12 +0300

    getting year from client and sending it to server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,23 @@
-import React, {useState} from 'react';
-import './App.css';
-import Calendar from "./components/calendar";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import Calendar, { Day } from "./components/calendar";
 
 function App() {
-    const [calendar, setCalendar] = useState<object[][] | null>(null)
-    const currentYear = new Date().getFullYear()
-    return (
-        <Calendar year={currentYear} calendar={calendar} setCalendar={setCalendar}/>
-    );
+  const [calendar, setCalendar] = useState<Day[][] | null>(null);
+  const currentYear = new Date().getFullYear();
+  useEffect(() => {
+    axios.get(`/${currentYear}`).then((res) => {
+      setCalendar(res.data);
+    });
+  }, [currentYear]);
+  return (
+    <>
+      <h1 className={"year"}>{currentYear}</h1>
+      <hr />
+      <Calendar calendar={calendar} />
+    </>
+  );
 }
 
 export default App;
