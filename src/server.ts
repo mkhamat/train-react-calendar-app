115d920dd8c24325076c0677451e3573466208c9
mkhamat <myxa@xakep.org>
2020-10-24 15:20:25 +0300

    Prettier did it, changed express port

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import express from 'express'
-import path from 'path'
+import express from "express";
+import path from "path";
 import generateCalendar from "./calendar";
-const app = express()
-app.listen(3000)
+const app = express();
+app.listen(3001);
 
-app.use(express.static(path.join(__dirname,'../client/build')))
-app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'../client/build/index.html'))
-})
-app.get('/:year', (req,res)=>{
-    res.send(JSON.stringify(generateCalendar(~~req.params.year)))
-})
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+app.get("/:year", (req, res) => {
+  res.send(JSON.stringify(generateCalendar(~~req.params.year)));
+});
