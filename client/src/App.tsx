d410f1e5754a79205cfcbdc633c42a60455e27e5
Mkhamat <myxa@xakep.org>
2020-11-29 19:27:49 +0300

    getting rid of useless server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,19 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import "./App.css";
-import Calendar, { Day } from "./components/calendar";
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+import "./App.css"
+import Calendar, { Day } from "./components/calendar"
+import generateCalendar from "./calendar"
 
 function App() {
-  const [calendar, setCalendar] = useState<Day[][] | null>(null);
-  const currentYear = new Date().getFullYear();
+  const [calendar, setCalendar] = useState<Day[][] | null>(null)
+  const [year, setYear] = useState(new Date().getFullYear())
   useEffect(() => {
-    axios.get(`/${currentYear}`).then((res) => {
-      setCalendar(res.data);
-    });
-  }, [currentYear]);
-  return <Calendar year={currentYear} calendar={calendar} />;
+    setCalendar(generateCalendar(year))
+    // axios.get(`/${year}`).then((res) => {
+    //   setCalendar(res.data)
+    // })
+  }, [year])
+  return <Calendar year={year} calendar={calendar} setYear={setYear} />
 }
 
-export default App;
+export default App
